a783943d4c54170849a3e01a6a95b5bca2bebf91
mosalman1379 <ali.mosalman44@gmail.com>
2021-07-14 14:34:23 +0430

    customize user model and register user feature added

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    """
+    User form registration
+    """
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    confirmPassword = forms.CharField(widget=forms.PasswordInput, label='Password')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_confirmPassword(self):
+        clean = self.cleaned_data
+        if clean['password'] != clean['confirmPassword']:
+            raise forms.ValidationError('Password don\' match')
+        return clean['password']
