6ecc1cc0ce8dc716a6c66da6cdb3de92c648b012
mosalman1379 <ali.mosalman44@gmail.com>
2021-07-14 19:16:56 +0430

    create Image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='image_created')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, blank=True)
+    url = models.URLField()
+    image = models.ImageField('images/')
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(User, related_name='images_liked', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
